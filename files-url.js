5e5582fb896c51f1c0fd7b8ebd25136a27293c2d
Nnanyielugo <kz.onyegbule@gmail.com>
2020-08-23 16:54:13 +0100

    add track names

--- files-url.js
+++ files-url.js
@@ -1,44 +1,58 @@
 const filesUrl = [
   {
     url: './Mumford/01.mp3',
+    trackName: '42'
   },
   {
-    url: './Mumford/02.mp3'
+    url: './Mumford/02.mp3',
+    trackName: 'Guilding Light'
   },
   {
     url: './Mumford/03.mp3',
+    trackName: 'Woman'
   },
   {
-    url: './Mumford/04.mp3'
+    url: './Mumford/04.mp3',
+    trackName: 'Beloved'
   },
   {
     url: './Mumford/05.mp3',
+    trackName: 'The Wild'
   },
   {
-    url: './Mumford/06.mp3'
+    url: './Mumford/06.mp3',
+    trackName: 'October Skies'
   },
   {
     url: './Mumford/07.mp3',
+    trackName: 'Slip Away'
   },
   {
-    url: './Mumford/08.mp3'
+    url: './Mumford/08.mp3',
+    trackName: 'Rose of Sharon'
   },
   {
     url: './Mumford/09.mp3',
+    trackName: 'Picture You'
   },
   {
-    url: './Mumford/10.mp3'
+    url: './Mumford/10.mp3',
+    trackName: 'Darkness Visible'
   },
   {
     url: './Mumford/11.mp3',
+    trackName: 'If I say'
   },
   {
-    url: './Mumford/12.mp3'
+    url: './Mumford/12.mp3',
+    trackName: 'Wild Heart'
   },
   {
     url: './Mumford/13.mp3',
+    trackName: 'Forever'
   },
   {
-    url: './Mumford/14.mp3'
+    url: './Mumford/14.mp3',
+    trackName: 'Delta' 
   }
 ];
