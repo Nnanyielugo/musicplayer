5d29245c05d9c44ac37d3d78cc9b11d1b356e46c
Nnanyielugo <kz.onyegbule@gmail.com>
2020-08-23 16:53:12 +0100

    move tracklist to own folder

---
+++ files-url.js
@@ -0,0 +1,44 @@
+const filesUrl = [
+  {
+    url: './Mumford/01.mp3',
+  },
+  {
+    url: './Mumford/02.mp3'
+  },
+  {
+    url: './Mumford/03.mp3',
+  },
+  {
+    url: './Mumford/04.mp3'
+  },
+  {
+    url: './Mumford/05.mp3',
+  },
+  {
+    url: './Mumford/06.mp3'
+  },
+  {
+    url: './Mumford/07.mp3',
+  },
+  {
+    url: './Mumford/08.mp3'
+  },
+  {
+    url: './Mumford/09.mp3',
+  },
+  {
+    url: './Mumford/10.mp3'
+  },
+  {
+    url: './Mumford/11.mp3',
+  },
+  {
+    url: './Mumford/12.mp3'
+  },
+  {
+    url: './Mumford/13.mp3',
+  },
+  {
+    url: './Mumford/14.mp3'
+  }
+];
